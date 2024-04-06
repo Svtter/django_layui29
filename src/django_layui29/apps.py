fb0710b183d2c2b3727c2f11420416d3553a0fee
svtter <svtter@qq.com>
2024-04-06 13:13:27 +0800

    fix: error of app_name

--- src/django_layui29/apps.py
+++ src/django_layui29/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class DjangoLayuiConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'django_layui'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "django_layui29"
